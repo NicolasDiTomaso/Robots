19adfe77f05d33eb4ef4f2391c05bbdb9847d8ce
javierlara <javi29c@hotmail.com>
2015-07-07 00:50:28 -0300

    More testing with net config. Normalizing data

--- Robots_TP/src/main.py
+++ Robots_TP/src/main.py
@@ -1,9 +1,12 @@
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.structure import TanhLayer
 import csv
+from pybrain.supervised.trainers import *
+import numpy as np
 
-ds = SupervisedDataSet(4, 1)
+ds = SupervisedDataSet(3, 1)
 
 fin = open('train_format2_processed.csv','r')
 fin.readline()
@@ -12,32 +15,55 @@ print "Reading data"
 i = 0
 testData = []
 for line in fin.readlines():
-	if i < 100000: # tomo una muestra de datos para el entrenamiento
+	if i < 50000: # tomo una muestra de datos para el entrenamiento
 		trainRow = [float(x) for x in line.strip().split(',')]
-		ds.addSample(tuple(trainRow[:4]),tuple(trainRow[4:]))
+		ds.addSample(tuple(trainRow[:3]),tuple(trainRow[3:]))
 		i = i + 1
-	elif i < 200000: # tomo una muestra de datos para las pruebas
+	elif i < 100000: # tomo una muestra de datos para las pruebas
 		testRow = [float(x) for x in line.strip().split(',')]
 		testData.append(testRow) 
 		i = i + 1
 	else: 
 		break
 
-net = buildNetwork(ds.indim,8,8,ds.outdim,recurrent=True)
-t = BackpropTrainer(net, learningrate=0.001,momentum=0.05,verbose=True)
+# normalizing
+i = np.array([d[0] for d in ds])
+i /= np.max(np.abs(i),axis=0)
+o = np.array([d[1] for d in ds])
+o /= np.max(np.abs(o),axis=0)
+
+test = np.array([row for row in testData])
+test /= np.max(np.abs(test), axis=0)
+
+nds = SupervisedDataSet(3, 1)
+for ix in range(len(ds)):
+    nds.addSample( i[ix], o[ix])
+
+#net = buildNetwork(ds.indim,4,3,ds.outdim,recurrent=True)
+net = buildNetwork(nds.indim,5,nds.outdim, recurrent=True)
+#t = BackpropTrainer(net, learningrate=0.001,momentum=0.05,verbose=True)
+#t = BackpropTrainer(net,ds,verbose=True)
+t = RPropMinusTrainer(net,verbose=True)
 
 print "Training"
-t.trainUntilConvergence(ds, verbose=True, maxEpochs=10)
+#t.trainUntilConvergence(nds,verbose=True, maxEpochs=30)
+t.trainOnDataset(nds,5)
+#t.testOnData(nds, verbose=True)
 
 print "Testing"
 fout = open('results.csv','w')
 writer = csv.writer(fout, delimiter=',')
 correct = 0
 total = 0
-for testRow in testData:
-	predicted = round(net.activate(testRow[:4])[0])
-	writer.writerow([testRow, predicted])
-	if predicted == testRow[4]:
+for testRow in test:
+	predicted = net.activate(testRow[:3])[0]
+	roundedPredicted = round(predicted)
+	writer.writerow([testRow, roundedPredicted, predicted])
+	if roundedPredicted == testRow[3]:
 		correct = correct + 1
 	total = total + 1
 print "Precission: " + str(float(correct)/float(total))
+
+
+
+# Precission: 0.614815423826
--- Robots_TP/src/preprocessData.py
+++ Robots_TP/src/preprocessData.py
@@ -1,14 +1,34 @@
 import csv
+from random import shuffle
 
-fin = open('/home/lara/FIUBA/ROBOTS/data_format2/train_format2.csv','r')
-fout = open('train_format2_processed2.csv','w')
+fin = open('data_format2/train_format2.csv','r')
+fout = open('train_format2_processed.csv','w')
 writer = csv.writer(fout, delimiter=',')
 
+# manteniendo header
+line = fin.readline()
+writer.writerow(line.strip().split(',')[1:5])
+
+length = 50000
+
 # Preprocesamiento: se sacan registros con valores vacios o nulos y el activity_log y el user_id
+repeatBuyers = []
+nonRepeatBuyers = []
 print "Starting"
 for line in fin.readlines():
 	data = [x for x in line.strip().split(',')[1:5] if x != '']
-	if len(data) < 4:
+	if len(data) < 4 or data[0] == '0' or data[1] == '2' or data[3] == '-1': # removing unknown data
 		continue
-	writer.writerow(data)
+	if data[3] == '1' and len(repeatBuyers) < length: # 5000 de repetidos
+		repeatBuyers.append(data)
+	if data[3] == '0' and len(nonRepeatBuyers) < length: # 5000 de no repetidos
+		nonRepeatBuyers.append(data);
+	
+	if len(repeatBuyers) >= length and len(nonRepeatBuyers) >= length:
+		break
+
+toWrite = repeatBuyers + nonRepeatBuyers
+shuffle(toWrite)
+writer.writerows(toWrite)
+
 print "Finished"
